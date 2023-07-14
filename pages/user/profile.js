95aeeac9444064b1eb9ecc0ae03540bf2488d0d3
Balaganesh K <k.balaganesh26@gmail.com>
2023-07-14 13:15:41 +0530

    updated the profile page

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -19,96 +19,89 @@ import LogoMailStar from '@/assets/mailstar.svg';
 const Profile = () => {
   return (
     <div className="block  mobile-lg:max-w-[64rem] w-full    px-2 mobile-md:px-4 mobile-lg:px-6 md:px-8 py-8 mt-[10rem] mx-auto ">
-      <div className="flex flex-col gap-6 ">
-        <div className=" flex flex-row gap-6">
-          {/* Profile header */}
-          <div className="w-full basis-[70%] bg-white rounded-[10px] p-6 flex gap-6  border border-[#d7d7d7] relative">
-            <div className="w-[6rem] h-[6rem] bg-gray-400 border border-[#d7d7d7] rounded-[10px]"></div>
-            <div className="">
-              <h4 className="text-[1.5rem] font-CabinetGrotesk-Medium">Name</h4>
-              <p className="text-[1rem] leading-[1.9rem]">
-                @gabriellac • New York, NY, USA • she/her
-              </p>
-              <p className="text-[1rem] leading-[1.9rem]">
-                A startup enthusiast exploring creator economy
-              </p>
-              {/* Edit */}
-              <button className="absolute right-5 top-5">
-                <Image src={PencilLogo} alt="edit" width={24} height={24} />
-              </button>
-            </div>
-          </div>
-          <div className="w-full basis-[30%] p-6 border rounded-[10px] border-[#d7d7d7]  flex items-center justify-center bg-white">
-            <div className="flex flex-col w-full items-center justify-center">
-              <Image src={LogoRank} alt="rank" width={84} height={84} />
-              <p className="text-[#3870DD] text-[1rem]">Rank yet to achive</p>
-            </div>
+      <div className="grid grid-cols-1  mobile-lg:grid-cols-5  mobile-lg:grid-rows-3  md:grid-cols-7  md:grid-rows-2 gap-6 ">
+        {/* Profile header */}
+        <div className="w-full   mobile-lg:col-span-5 row-span-1 bg-white rounded-[10px] p-6 flex gap-6 flex-col mobile-lg:flex-row items-center mobile-lg:items-start border border-[#d7d7d7] relative">
+          <div className="w-[6rem] h-[6rem] flex-shrink-0 bg-gray-400 border border-[#d7d7d7] rounded-[10px]"></div>
+          <div className="text-center mobile-lg:text-left">
+            <h4 className="text-[1.5rem] font-CabinetGrotesk-Medium">Name</h4>
+            <p className="text-[1rem] leading-[1.9rem]">
+              @gabriellac • New York, NY, USA • she/her
+            </p>
+            <p className="text-[1rem] leading-[1.9rem]">
+              A startup enthusiast exploring creator economy
+            </p>
+            {/* Edit */}
+            <button className="absolute right-5 top-5">
+              <Image src={PencilLogo} alt="edit" width={24} height={24} />
+            </button>
           </div>
         </div>
-        <div className=" flex flex-row gap-6">
-          <div className="w-full basis-[70%] flex gap-6 ">
-            {/* Social links */}
-            <div className=" basis-[45%] p-6 bg-white border rounded-[10px] border-[#d7d7d7] relative">
-              <h4 className="text-[1.5rem] font-CabinetGrotesk-Medium">
-                Social
-              </h4>
-              <div className="flex flex-wrap gap-x-3 gap-y-4 mt-3">
-                <div>
-                  <Image src={LogoTwitter} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoTelegram} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image
-                    src={LogoInstagram}
-                    alt="rank"
-                    width={28}
-                    height={28}
-                  />
-                </div>
-                <div>
-                  <Image src={LogoLinkedin} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoMedium} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoYoutube} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoDribble} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoGithub} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoBehance} alt="rank" width={28} height={28} />
-                </div>
-                <div>
-                  <Image src={LogoTiktok} alt="rank" width={28} height={28} />
-                </div>
-              </div>
-              {/* Edit */}
-              <button className="absolute right-5 top-5">
-                <Image src={PencilLogo} alt="edit" width={24} height={24} />
-              </button>
-            </div>
-            <div className=" basis-[55%] p-6 bg-white border rounded-[10px] items-center flex gap-6 border-[#d7d7d7]">
-              <Image src={LogoBadge} alt="rank" width={96} height={96} />
-              <p className="text-[#FF8D1F] text-[1rem] leading-[1.5]">
-                You haven’t earned any badges yet
-              </p>
-            </div>
+
+        <div className="w-full mobile-lg:col-span-2 mobile-lg:row-span-1 p-6 border rounded-[10px] border-[#d7d7d7]  flex items-center justify-center bg-white ">
+          <div className="flex flex-col w-full items-center justify-center">
+            <Image src={LogoRank} alt="rank" width={84} height={84} />
+            <p className="text-[#3870DD] text-[1rem] text-center">
+              Rank yet to achive
+            </p>
           </div>
-          <div className="basis-[30%] bg-white w-full p-6 border rounded-[10px] border-[#d7d7d7]">
-            <div className="flex flex-col w-full items-center justify-center">
-              <Image src={LogoCoin} alt="rank" width={152} height={152} />
+        </div>
+        <div className="w-full grid-cols-1  mobile-lg:col-span-3 mobile-lg:row-span-2 md:row-span-1 md:col-span-5 gap-6 grid grid-rows-2 md:grid-rows-1 md:grid-cols-10 ">
+          {/* Social links */}
+          <div className=" p-6 md:col-span-4 bg-white border rounded-[10px] border-[#d7d7d7] relative">
+            <h4 className="text-[1.5rem] font-CabinetGrotesk-Medium">Social</h4>
+            <div className="flex flex-wrap gap-x-3 gap-y-4 mt-3">
+              <div>
+                <Image src={LogoTwitter} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoTelegram} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoInstagram} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoLinkedin} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoMedium} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoYoutube} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoDribble} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoGithub} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoBehance} alt="rank" width={28} height={28} />
+              </div>
+              <div>
+                <Image src={LogoTiktok} alt="rank" width={28} height={28} />
+              </div>
             </div>
+            {/* Edit */}
+            <button className="absolute right-5 top-5">
+              <Image src={PencilLogo} alt="edit" width={24} height={24} />
+            </button>
+          </div>
+          <div className="p-6 md:col-span-6  bg-white border rounded-[10px] items-center flex gap-[2%] border-[#d7d7d7]">
+            <Image src={LogoBadge} alt="rank" width={96} height={96} />
+            <p className="text-[#FF8D1F] text-[1rem] leading-[1.5]">
+              You haven’t earned any badges yet
+            </p>
+          </div>
+        </div>
+        <div className=" mobile-lg:col-span-2 bg-white w-full p-6 border rounded-[10px] border-[#d7d7d7]">
+          <div className="flex flex-col w-full h-full items-center justify-center">
+            <Image src={LogoCoin} alt="rank" width={152} height={152} />
           </div>
         </div>
       </div>
-      <div className="flex flex-row gap-6 mt-6">
+      {/* 2nd row */}
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-6">
         <div className="p-6 w-full bg-white border border-[#d7d7d7] rounded-[10px]">
           <h4 className="text-[2rem] font-CabinetGrotesk-Regular leading-[3rem]">
             256
@@ -143,6 +136,7 @@ const Profile = () => {
           </p>
         </div>
       </div>
+      {/* 3rd row */}
       <div className="p-[3rem] bg-white border border-[#d7d7d7] rounded-[10px] mt-6 flex items-end justify-center">
         <div className="flex flex-col items-center">
           <Image src={LogoMailStar} alt="rank" width={96} height={96} />
