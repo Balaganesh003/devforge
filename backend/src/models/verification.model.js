59b3fb73e5c476e8525db56f529619667b7f3e9e
Raghuram2003 <destrobroxd@gmail.com>
2024-06-23 19:14:41 +0530

    basic structute and user models designed

---
+++ backend/src/models/verification.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const VerificationSchema = mongoose.Schema({
+    userId : {type : mongoose.Schema.ObjectId,ref: 'users',required : true},
+    otp : {type : String,required : true},
+    expiryTime : {type : Date,default : Date.now()+360000}
+})
+
+export const Verification = mongoose.model('Verification',VerificationSchema);
